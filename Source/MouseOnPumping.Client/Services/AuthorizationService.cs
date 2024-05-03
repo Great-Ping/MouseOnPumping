edaa5cc4006c4aca1a5b770b91ac202ebc2cbb7b
Witchs-Cat <da18.06.03@yandex.ru>
2024-05-04 00:25:20 +0300

    Футер

--- Source/MouseOnPumping.Client/Services/AuthorizationService.cs
+++ Source/MouseOnPumping.Client/Services/AuthorizationService.cs
@@ -4,7 +4,6 @@ namespace MouseOnPumping.Services
 {
     public class AuthorizationService(MouseClient client)
     {
-
         public async Task AuthorizeAsync(string email, string password) 
         {
             await client.AuthorizationRequest(email, password);
